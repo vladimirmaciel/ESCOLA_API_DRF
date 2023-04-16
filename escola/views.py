758dc4e19211bb731d319606e715a69ef50d3429
Vlamidir Serra <vladimir.serra@itec.al.gov.br>
2023-04-16 00:17:06 -0300

    @vladimirmaciel Fixes #4

--- escola/views.py
+++ escola/views.py
@@ -1,8 +1,17 @@
 # from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework import viewsets
+from serializer import AlunoSerializer, CursoSerializer
+
+from .models import Aluno, Curso
 
 
-def alunos(request):
-    if request.method == "GET":
-        aluno = {'id':1, 'nome':'Luke'}
-    return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+
+class CursosViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
