8c65048343823bc4bdd6dbdca7eac1a5d9a5b979
Vlamidir Serra <vladimir.serra@itec.al.gov.br>
2023-04-15 23:44:47 -0300

    Criar os arquivo arquivo serializer.py  #1

--- escola/models.py
+++ escola/models.py
@@ -6,19 +6,18 @@ class Aluno(models.Model):
     rg = models.CharField(max_length=9)
     cpf = models.CharField(max_length=30)
     data_nascimento = models.DateField()
-    
+
     def __str__(self) -> str:
         return self.nome
-    
+
+
 class Curso(models.Model):
-    NIVEL = (
-        ('B', 'Básico'),
-        ('I', 'Intermediário'), 
-        ('A', 'Avançado')
-    )
+    NIVEL = (("B", "Básico"), ("I", "Intermediário"), ("A", "Avançado"))
     codigo_curso = models.CharField(max_length=10)
     descricao = models.CharField(max_length=100)
-    nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False, default='B')
-    
+    nivel = models.CharField(
+        max_length=1, choices=NIVEL, blank=False, null=False, default="B"
+    )
+
     def __str__(self) -> str:
         return self.descricao
