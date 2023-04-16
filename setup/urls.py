177418f104ed04d5a6fc6c6df304e18aa43680b9
Vlamidir Serra <vladimir.serra@itec.al.gov.br>
2023-04-16 01:40:33 -0300

    Issue Add o recurso de matrícula na API Fixes #11

--- setup/urls.py
+++ setup/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-from escola.views import AlunosViewSet, CursosViewSet
+from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet
 
 """definindo a rota principal """
 router = routers.DefaultRouter()
 router.register("alunos", AlunosViewSet, basename="Alunos")
 router.register("cursos", CursosViewSet, basename="Cursos")
+router.register("matricula", MatriculaViewSet, basename="Matriculas")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
